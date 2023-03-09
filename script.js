b0feb8bbf20a5e3f4007d3a1e632b0e9ed178159
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-03-08 20:37:24 -0600

    wow

---
+++ script.js
@@ -0,0 +1,20 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+let APIKey = "6c095c9bd22b608edb0a7591e5d691c9";
+let city = document.getElementById("cityinput").value;
+let requestUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+let btn = document.getElementById("btnn");
+
+
